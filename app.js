a43329c3716fce4d0805810f07527c95fa084f22
lgLuis <luis_unlz@yahoo.com.ar>
2020-12-04 20:36:41 -0300

    estilos correctos

--- app.js
+++ app.js
@@ -6,22 +6,21 @@ function renderPokemon(image){
 */
 
 //const url = `https://pokeapi.co/api/v2/pokemon/${id}/`
-const url = "https://pokeapi.co/api/v2/pokemon/10/"
+const urll = "https://pokeapi.co/api/v2/pokemon/10/"
 
-fetch(url)
+fetch(urll)
 .then((response)=> response.json())
 .then((data)=>{
     let element =document.querySelector(".aqui");
     element.innerHTML=`
     <img src="${data.sprites.front_default}"> </img>
-    <img src="${data.sprites.front_shiny}"> </img>
     <p>NOMBRE: ${data.name}</p>
     <p>NRO DE ID: ${data.id}</p>
     <p>EXPERIENCIA: ${data.base_experience}</p>
     <p>ALTURA: ${data.height}</p>
     <p>PESO: ${data.weight}</p>
-
-
+    <p>MOVIMIENTO: ${data.moves[2].move.name}</p>
+    <p>STATS: ${data.stats[1].stat.name}</p>
+    <br>
     `
-
 })
